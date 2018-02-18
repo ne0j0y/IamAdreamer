7803e322e9ab8e26c0040ad9872b5645541627c8
ne0j0y <neojoy@gmail.com>
2018-02-18 08:42:02 +0530

    Multiple Runs
    Handling the multiple runs with the same set of files.

--- validate.sh
+++ validate.sh
@@ -17,17 +17,29 @@ do
 vSearchFile="Final Sequence for the file type "$a
 finseq=$(awk -v serstr="$vSearchFile" '$0 ~ serstr {getline; print}' FILE_STATS.txt.tmp)
 
+#### To get the starting sequence from the previous run
+
+vSearchFile="Starting Sequence for the file type "$a
+startseq=$(awk -v serstr="$vSearchFile" '$0 ~ serstr {getline; print}' FILE_STATS.txt.tmp)
+
+#### To get the total number of files from the previous run
+
+vSearchFile="Total number of files of type "$a
+totfiles=$(awk -v serstr="$vSearchFile" '$0 ~ serstr {getline; print}' FILE_STATS.txt.tmp)
+
 
 if [ -f $a*gz ] || [ -f $a*DAT ]; then
 
 echo "Total number of files of type $a:" >> FILE_STATS.txt
 if [ $a == "CNUM" ]; then
 
-	ls -1a $a*DAT | awk -F"_" 'NR>=1{arr[$1$2]++}END{for (a in arr) print arr[a]}' >> FILE_STATS.txt
+	vCurrTotFiles=$(ls -1a $a*DAT | awk -F"_" 'NR>=1{arr[$1$2]++}END{for (a in arr) print arr[a]}') 
 else
 
-	ls -1a $a*gz | awk -F"_" 'NR>=1{arr[$1$2]++}END{for (a in arr) print arr[a]}' >> FILE_STATS.txt
+	vCurrTotFiles=$(ls -1a $a*gz | awk -F"_" 'NR>=1{arr[$1$2]++}END{for (a in arr) print arr[a]}') 
 fi
+echo $vCurrTotFiles >> FILE_STATS.txt
+
 
 ### finding the starting sequence
 
@@ -44,25 +56,35 @@ fi
 echo "Final Sequence for the file type "$a":" >> FILE_STATS.txt
 if [ $a == "CNUM" ]; then
 
-	ls -1a $a*DAT | awk -F"_" '{print $5}' | awk -F"." '{print $1}'| awk 'NR == 1 || $1 > max {line = $0; max = $1}END{print line}' >> FILE_STATS.txt
+	vCurrFinSeq=$(ls -1a $a*DAT | awk -F"_" '{print $5}' | awk -F"." '{print $1}'| awk 'NR == 1 || $1 > max {line = $0; max = $1}END{print line}') 
 else
-	ls -1a $a*gz | awk -F"_" '{print $3}' | awk -F"." '{print $1}'| awk 'NR == 1 || $1 > max {line = $0; max = $1}END{print line}' >> FILE_STATS.txt
+	vCurrFinSeq=$(ls -1a $a*gz | awk -F"_" '{print $3}' | awk -F"." '{print $1}'| awk 'NR == 1 || $1 > max {line = $0; max = $1}END{print line}')
 fi
+
+echo $vCurrFinSeq >> FILE_STATS.txt 
 
 ### finding the missing sequence
 echo "Missing Files for the file type "$a":" >> FILE_STATS.txt
-if [ $a ==  "CNUM" ]; then
+if [ "$vCurrFinSeq" -eq "$finseq" ] && [ "$vCurrTotFiles" -eq "$totfiles" ]; then   ##### To handle multiple runs with the same set of files.
 
-	vMiss=$(ls -1a $a*DAT | awk -F"_" '{print $5}' | awk -F"." '{print $1}'| awk -v p="$finseq" '$1!=p+1{print p+1"-"$1-1}{p=$1}')
+	vSrchFileMissSeq="Missing Files for the file type "$a
+	vMissSeq=$(awk -v serstr="$vSrchFileMissSeq" '$0 ~ serstr {getline; print}' FILE_STATS.txt.tmp)
+	echo $vMissSeq >> FILE_STATS.txt
 
 else
+	if [ $a ==  "CNUM" ]; then
 
-	vMiss=$(ls -1a $a*gz | awk -F"_" '{print $3}' | awk -F"." '{print $1}'|awk -v p="$finseq" '$1!=p+1{print p+1"-"$1-1}{p=$1}') 
-fi
-if [ -z "$vMiss" ]; then
-	echo "No Missing Files" >> FILE_STATS.txt
-else
-	echo $vMiss >> FILE_STATS.txt
+		vMiss=$(ls -1a $a*DAT | awk -F"_" '{print $5}' | awk -F"." '{print $1}'| awk -v p="$finseq" '$1!=p+1{print p+1"-"$1-1}{p=$1}')
+
+	else
+
+		vMiss=$(ls -1a $a*gz | awk -F"_" '{print $3}' | awk -F"." '{print $1}'|awk -v p="$finseq" '$1!=p+1{print p+1"-"$1-1}{p=$1}') 
+	fi
+	if [ -z "$vMiss" ]; then
+		echo "No Missing Files" >> FILE_STATS.txt
+	else
+		echo $vMiss >> FILE_STATS.txt
+	fi
 fi
 
 echo "######################################################################################" >> FILE_STATS.txt
